8b9150d6c9968abc683b4afb59809eacfd457d4c
Thomasin Abraham <thomasinabraham@me.com>
2017-09-27 13:15:42 +1300

    Integrate redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+
+import reducers from './reducers'
 
 import App from './components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-      <App />,
-      document.getElementById('app')
-      )
+    <Provider store={ createStore(reducers) }>
+      <App />
+    </Provider>,
+    document.getElementById('app')
+  )
 })
